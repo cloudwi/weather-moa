83fe419f177e01da0d425ceb639c5358998ced63
cloudwi <cloudwi@naver.com>
2022-08-14 04:42:14 +0900

    test
    Signed-off-by: cloudwi <cloudwi@naver.com>

--- index.js
+++ index.js
@@ -1,11 +1,73 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const axios = require('axios')
+const moment = require('moment')
+const apiKeyData = require('./key/apiKeyData')
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+let koArr
+let usArr
+
+app.set('view enfine', 'ejs')
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.sendFile(__dirname + '/page/index.html')
 })
 
-app.listen(port, () => {
-  console.log('listening on 8080')
+app.get('/Busan', async (req, res) => {
+  await axios(
+    'https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst?' +
+      'serviceKey=' +
+      apiKeyData.krServiceKey +
+      '&pageNo=1&numOfRows=60&dataType=JSON&base_date=' +
+      moment().subtract(1, 'hour').format('YYYYMMDD') +
+      '&base_time=' +
+      moment().subtract(1, 'hour').format('HH00') +
+      '&nx=' +
+      apiKeyData.Busan.nx +
+      '&ny=' +
+      apiKeyData.Busan.ny
+  )
+    .then((response) => {
+      koArr = response.data.response.body.items.item.filter(
+        (element) =>
+          element.category === 'SKY' ||
+          element.category === 'T1H' ||
+          element.category === 'RN1'
+      )
+
+      koArr.forEach((element) => {
+        element.fcstTime = [
+          element.fcstTime.slice(0, 2),
+          ':',
+          element.fcstTime.slice(2),
+        ].join('')
+      })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+
+  await axios(
+    'https://api.openweathermap.org/data/2.5/forecast?lat=' +
+      apiKeyData.Busan.lat +
+      '&lon=' +
+      apiKeyData.Busan.lon +
+      '&appid=' +
+      apiKeyData.usServiceKey +
+      '&units=metric&cnt=6'
+  )
+    .then((response) => {
+      usArr = response.data.list
+      usArr.forEach((element) => {
+        element.dt_txt = element.dt_txt.slice(11, 16)
+        element.main.temp = Math.floor(element.main.temp)
+      })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+
+  res.render('test.ejs', { koArr: koArr, usArr: usArr })
 })
