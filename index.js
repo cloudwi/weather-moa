baa0c4d2b9898a0e42cbc4eab2276e8c9e0d03df
bbangHo <sonsins1@naver.com>
2022-08-14 08:43:46 +0900

    .

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = /*process.env.PORT ||*/ 3000
+const port = process.env.PORT || 3000
 const axios = require('axios')
 const moment = require('moment')
 const apiKeyData = require('./key/apiKeyData')
