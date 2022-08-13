3d1879609eab9eeaf03f8d49eff96070bcac8770
bbangHo <sonsins1@naver.com>
2022-08-14 08:44:20 +0900

    .

--- keys/apiKeyData.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  krServiceKey:
-    '5R9T1SG8PJ%2Bx2S1BbYxXnVQGHUOnIj4ij6DhEEU8LdEIjlN9XZKG7239DKE%2FyJgzfl5f%2B6a72txWs98IaJhOiA%3D%3D',
-  opnWeterServiceKey: 'c7c793d38c781c47682fd00e09f1a7da',
-  Busan: { nx: '98', ny: '76', lat: '35.1333', lon: '129.05' },
-  Seoul: { nx: '60', ny: '27', lat: '37.5683', lon: '126.9778' }
-}
