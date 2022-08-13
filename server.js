e0bfc89d52aa9bc7f04cd3e878ba26a3477f89b2
cloudwi <cloudwi@naver.com>
2022-08-14 03:25:38 +0900

    데이터 준비 끝
    Signed-off-by: cloudwi <cloudwi@naver.com>

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
 const weatherData = require('./utils/weatherData')
+const axios = require('axios')
+const moment = require('moment')
+const apiKeyData = require('./key/apiKeyData')
+const e = require('express')
+
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+
+let koArr
+let usArr
 
 app.set('view enfine', 'ejs')
 
@@ -25,22 +33,59 @@ MongoClient.connect(
   }
 )
 
-weatherData(app, 'Seoul')
+app.get('/Busan', async (req, res) => {
+  await axios(
+    'https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst?' +
+      'serviceKey=' +
+      apiKeyData.krServiceKey +
+      '&pageNo=1&numOfRows=60&dataType=JSON&base_date=' +
+      moment().subtract(1, 'hour').format('YYYYMMDD') +
+      '&base_time=' +
+      moment().subtract(1, 'hour').format('HH00') +
+      '&nx=' +
+      apiKeyData.Busan.nx +
+      '&ny=' +
+      apiKeyData.Busan.ny
+  )
+    .then((response) => {
+      koArr = response.data.response.body.items.item.filter(
+        (element) =>
+          element.category === 'SKY' ||
+          element.category === 'T1H' ||
+          element.category === 'RN1'
+      )
 
-app.get('/pet', (req, res) => {
-  res.send('안녕하세요')
-  console.log()
+      koArr.forEach((element) => {
+        element.fcstTime = [
+          element.fcstTime.slice(0, 2),
+          ':',
+          element.fcstTime.slice(2),
+        ].join('')
+      })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+
+  await axios(
+    'https://api.openweathermap.org/data/2.5/forecast?lat=' +
+      apiKeyData.Busan.lat +
+      '&lon=' +
+      apiKeyData.Busan.lon +
+      '&appid=' +
+      apiKeyData.usServiceKey +
+      '&units=metric&cnt=6'
+  )
+    .then((response) => {
+      usArr = response.data.list
+      usArr.forEach((element) => {
+        element.dt_txt = element.dt_txt.slice(11, 16)
+        element.main.temp = Math.floor(element.main.temp)
+      })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+
+  res.render('test.ejs', { koArr: koArr, usArr: usArr })
 })
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-})
-
-// app.get('/weather', (req, res) => {
-//   db.collection('data')
-//     .find()
-//     .toArray((error, result) => {
-//       console.log(result)
-//       res.render('test.ejs', { datas: arr })
-//     })
-// })
--- utils/weatherData.js
+++ utils/weatherData.js
@@ -1,64 +1,12 @@
 const apiKeyData = require('../key/apiKeyData')
-const moment = require('moment')
-const axios = require('axios')
 
 module.exports = weatherData = (app, data) => {
-  let koArr
-  let usArr
-  axios(
-    'https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst?' +
-      'serviceKey=' +
-      apiKeyData.krServiceKey +
-      '&pageNo=1&numOfRows=60&dataType=JSON&base_date=' +
-      moment().subtract(1, 'hour').format('YYYYMMDD') +
-      '&base_time=' +
-      moment().subtract(1, 'hour').format('HH00') +
-      '&nx=' +
-      apiKeyData[data].nx +
-      '&ny=' +
-      apiKeyData[data].ny
-  )
-    .then((res) => {
-      koArr = res.data.response.body.items.item.filter(
-        (element) =>
-          element.category === 'SKY' ||
-          element.category === 'T1H' ||
-          element.category === 'PTY'
-      )
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-
-  axios(
-    'https://api.openweathermap.org/data/2.5/forecast?lat=' +
-      apiKeyData[data].lat +
-      '&lon=' +
-      apiKeyData[data].lon +
-      '&appid=' +
-      apiKeyData.usServiceKey +
-      '&units=metric&cnt=6'
-  )
-    .then((res) => {
-      usArr = res.data.list
-
-      // const arr = res.data.response.body.items.item.filter(
-      //   (element) =>
-      //     element.category === 'SKY' ||
-      //     element.category === 'T1H' ||
-      //     element.category === 'PTY'
-      // )
-      // app.get('/weather', (req, res) => {
-      //   res.render('test.ejs', { arr: arr })
-      // })
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-
-  app.get('/weather', (req, res) => {
+  
+  app.get('weather', (req, res) => {
     res.render('test.ejs', { koArr: koArr, usArr: usArr })
   })
+
+  
 }
 
 //https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst?serviceKey=5R9T1SG8PJ%2Bx2S1BbYxXnVQGHUOnIj4ij6DhEEU8LdEIjlN9XZKG7239DKE%2FyJgzfl5f%2B6a72txWs98IaJhOiA%3D%3D&pageNo=1&numOfRows=1000&dataType=JSON&base_date=20220813&base_time=2100&nx=55&ny=127
