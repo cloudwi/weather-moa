e8d28844b3d2c4659460e00602325f88c46788b2
cloudwi <cloudwi@naver.com>
2022-08-13 19:20:14 +0900

    html에 data 뽑기 성공
    Signed-off-by: cloudwi <cloudwi@naver.com>

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require('express')
 const { Db } = require('mongodb')
 const app = express()
+app.set('view enfine', 'ejs')
 
 const MongoClient = require('mongodb').MongoClient
 
+//데이터 베이스 정의
 var db
+
 MongoClient.connect(
   'mongodb+srv://cloudwi:MN77868!!!@cluster0.gc6u716.mongodb.net/?retryWrites=true&w=majority',
   (error, client) => {
     if (error) {
-      console.log('오류발생')
+      return console.log('오류발생')
     }
+
     db = client.db('weather')
-    db.collection('data').insertOne({ test: 'test' })
+    // db.collection('data').insertOne({ test: 'test' })
+
+    app.listen(8080, () => {
+      console.log('Server Start')
+    })
   }
 )
 
@@ -21,5 +29,14 @@ app.get('/pet', (req, res) => {
 })
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/page/index.html')
+  res.sendFile(__dirname + '/views/index.html')
+})
+
+app.get('/weather', (req, res) => {
+  db.collection('data')
+    .find()
+    .toArray((error, result) => {
+      console.log(result)
+      res.render('test.ejs', { datas: result })
+    })
 })
