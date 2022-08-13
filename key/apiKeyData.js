5281e563515595d6e1ccc6d548cdb5b985756a2f
cloudwi <86584887+cloudwi@users.noreply.github.com>
2022-08-14 03:37:30 +0900

    Delete key directory

--- key/apiKeyData.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  krServiceKey:
-    '5R9T1SG8PJ%2Bx2S1BbYxXnVQGHUOnIj4ij6DhEEU8LdEIjlN9XZKG7239DKE%2FyJgzfl5f%2B6a72txWs98IaJhOiA%3D%3D',
-  Busan: { nx: '98', ny: '76', lat: '35.1799528', lon: '129.0752365' },
-  Seoul: { nx: '60', ny: '27', lat: '37.5666791', lon: '126.9782914' },
-  usServiceKey: '83773977931f66e6bf1e7dec05bc5868',
-}
