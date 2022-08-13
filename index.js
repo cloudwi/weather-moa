c82f025758e1548e30247f362ea8da2e229658ed
cloudwi <cloudwi@naver.com>
2022-08-14 04:36:27 +0900

    test
    Signed-off-by: cloudwi <cloudwi@naver.com>

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
-app.listen(8080, () => {
+app.listen(port, () => {
   console.log('listening on 8080')
 })
